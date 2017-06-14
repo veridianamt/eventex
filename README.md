9c1356d081914327018a12ed9fdd3c81a8a75fea
Veridiana Mantecom <vmantecom@options.qss.com.br>
2017-06-14 16:48:49 -0300

    Refatora geral

---
+++ README.md
@@ -0,0 +1,40 @@
+# Eventex 
+
+Sistema de Eventos encomendado pela Morena.
+
+## Como desenvolver?
+
+1. Clone o repositório.
+2. Crie um virtualenv com Python 3.6.
+3. Ative o seu virtualenv.
+4. Instale as dependências.
+5. Configure a instância com o .env.
+6. Execute os testes.
+
+```console
+git clone git@github.com:veridianamt/eventex.git wttd
+cd wttd
+python -m venv .wttd
+call .wttd/Scripts/activate
+pip intall -r requirements.txt
+copy contrib/env-sample .env
+python manage.py test
+```
+
+## Como fazer o deploy?
+1. Crie uma instância no heroku.
+2. Envie as configurações para o heroku.
+3. Define uma SECRET_KEY segura para a instância.
+4. Defina DEBUG=False
+5. Configure o serviço de email.
+6. Envie o código para o heroku.
+
+```console
+heroku create minhainstância
+heroku config:push
+heroku config:set SECRET_KEY=python contrib/secret_gen.py
+heroku config:set DEBUG=False
+# configuro o email
+heroku git push master --force
+```
+
---
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,32 @@
+from django.core import mail
+from django.test import TestCase
+
+
+class SubscribePostValid(TestCase):
+    def setUp(self):
+        data = dict(name='Veridiana Mt', cpf='12345678901',
+                    email='veridiana@mt.net', phone='(19)98877-2233')
+        self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+    def test_subscription_email_subject(self):
+        expect = 'Confirmação de inscrição'
+        self.assertEqual(expect, self.email.subject)
+
+    def test_subscription_email_from(self):
+        expect = 'contato@eventex.com.br'
+        self.assertEqual(expect, self.email.from_email)
+
+    def test_subscription_email_to(self):
+        expect = ['contato@eventex.com.br', 'veridiana@mt.net']
+        self.assertEqual(expect, self.email.to)
+
+    def test_subscription_email_body(self):
+        contents = ['Veridiana Mt',
+                    '12345678901',
+                    'veridiana@mt.net',
+                    '(19)98877-2233']
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)
+
