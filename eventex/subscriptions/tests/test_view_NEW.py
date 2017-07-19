c1f263f371a3231cd2bc2c0cc7f13493d1e0c6d7
Veridiana Mantecom <vmantecom@options.qss.com.br>
2017-07-19 13:47:57 -0300

    Email ou telefone opcionais

--- eventex/subscriptions/tests/test_view_NEW.py
+++ eventex/subscriptions/tests/test_view_NEW.py
@@ -82,6 +82,12 @@ class SubscriptionsNewPostInvalid(TestCase):
         self.assertFalse(Subscription.objects.exists())
 
 
+class TemplateRegressionTest(TestCase):
+    def test_template_has_non_fields_errors(self):
+        invalid_data = dict(name='Veridiana Mantecon', cpf='12345678901')
+        response = self.client.post(r('subscriptions:new'), invalid_data)
+        self.assertContains(response, '<ul class="errorlist nonfield">')
+
 
 
 
