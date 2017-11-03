f49c518205d9485bca1fbaacb9140b2db62e5693
Veridiana Mantecom <vmantecom@options.qss.com.br>
2017-11-03 14:22:55 -0200

    Keynote speakers

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -16,3 +16,22 @@ class HomeTest(TestCase):
     def test_subscrption_link(self):
         expected = 'href="{}"'.format(r('subscriptions:new'))
         self.assertContains(self.response, expected)
+
+    def test_speakers(self):
+        """must show keynote speakers"""
+        contents = [
+            'Grace Hopper',
+             'http://hbn.link/hopper-pic',
+             'Alan Turing',
+             'http://hbn.link/turing-pic'
+             ]
+
+        for expected in contents:
+            with self.subTest():
+                 self.assertContains(self.response, expected)
+
+
+    def test_speakers_link(self):
+        expected = 'href="{}#speakers"'.format(r('home'))
+        self.assertContains(self.response, expected)
+
